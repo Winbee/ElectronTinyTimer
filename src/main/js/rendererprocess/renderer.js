d3874a11c9842d61fa4692b0fe939f702284f2ef
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2017-06-05 15:21:18 +0200

    Add a reset button

--- src/main/js/rendererprocess/renderer.js
+++ src/main/js/rendererprocess/renderer.js
@@ -2,23 +2,40 @@ const $ = require('jquery');
 
 
 const $loadingBar = $('#loading-bar');
+const $resetButton = $('#reset-button');
 
 // Set the date we're counting down to
 const durationInMs = 25 * 60000;
-const finalTime = new Date().getTime() + durationInMs ;
+let finalTime;
+let id;
 
-const id = setInterval(frame, 1000);
+start();
+
+function reset() {
+    clearInterval(id);
+    $loadingBar.css('width', '100%');
+    start();
+}
+
+function start() {
+    finalTime = new Date().getTime() + durationInMs;
+    id = setInterval(frame, 1000);
+}
+
 function frame() {
     // Get todays date and time
     const now = new Date().getTime();
 
     // Find the distance between now an the count down date
-    const ratioLeftToFinalTime = (finalTime - now)/durationInMs;
+    const ratioLeftToFinalTime = (finalTime - now) / durationInMs;
 
-    console.log(ratioLeftToFinalTime);
     if (ratioLeftToFinalTime < 0) {
         clearInterval(id);
     } else {
         $loadingBar.css('width', ratioLeftToFinalTime * 100 + '%');
     }
 }
+
+$resetButton.on('click', () => {
+    reset();
+});
