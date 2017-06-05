f96e337c671d520bdc8ced26a4852c5324f3b0ca
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2017-06-05 15:49:17 +0200

    Add notification

--- src/main/js/rendererprocess/renderer.js
+++ src/main/js/rendererprocess/renderer.js
@@ -5,13 +5,17 @@ const $loadingBar = $('#loading-bar');
 const $resetButton = $('#reset-button');
 
 // Set the date we're counting down to
-const durationInMs = 25 * 60000;
+const durationInMin = 25;
+const durationInMs = durationInMin * 60000;
 let finalTime;
 let id;
 
 start();
 
 function reset() {
+    new Notification('Tiny Timer reset', {
+        body: `Tiny timer has been reset to ${durationInMin} minutes.`
+    });
     clearInterval(id);
     $loadingBar.css('width', '100%');
     start();
@@ -31,6 +35,9 @@ function frame() {
 
     if (ratioLeftToFinalTime < 0) {
         clearInterval(id);
+        new Notification('Tiny Timer finished', {
+            body: `${durationInMin} minutes have passed.`
+        });
     } else {
         $loadingBar.css('width', ratioLeftToFinalTime * 100 + '%');
     }
